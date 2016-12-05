b401b8b437afcc89de1b449a6ed5f455f0308184
Peter <qnguyen523@gmail.com>
2016-12-05 15:06:25 -0800

    deploy heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,12 +58,13 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address = 'smtp.gmail.com',
-    port = 587,
-    user_name = ' sjsutextbookexchange@gmail.com',
-    password = 'railscoder12345',
-    authentication = 'plain',
-    enable_starttls_auto = true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: '  spartansBookExchange@gmail.com',
+    password: 'railscoder12345',
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
